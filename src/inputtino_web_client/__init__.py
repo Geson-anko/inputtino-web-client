85d0d21300c99234cddad0654cff5dfa6859b874
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-02-02 06:10:57 +0000

    Add KeyCode, MouseButton, and ScrollDirection to module exports in inputtino_web_client

--- src/inputtino_web_client/__init__.py
+++ src/inputtino_web_client/__init__.py
@@ -1,8 +1,9 @@
 from importlib import metadata
 
-from .keyboard import Keyboard
+from .keyboard import Keyboard, KeyCode
+from .models import MouseButton, ScrollDirection
 from .mouse import Mouse
 
 __version__ = metadata.version(__name__.replace("_", "-"))
 
-__all__ = ["Mouse", "Keyboard"]
+__all__ = ["Mouse", "Keyboard", "KeyCode", "MouseButton", "ScrollDirection"]
