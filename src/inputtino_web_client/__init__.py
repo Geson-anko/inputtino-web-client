4f9fdb04633fd3c2c77d730eca032b688a77883d
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-02-02 05:49:03 +0000

    Add Mouse and Keyboard to module exports in inputtino_web_client

--- src/inputtino_web_client/__init__.py
+++ src/inputtino_web_client/__init__.py
@@ -1,3 +1,8 @@
 from importlib import metadata
 
+from .keyboard import Keyboard
+from .mouse import Mouse
+
 __version__ = metadata.version(__name__.replace("_", "-"))
+
+__all__ = ["Mouse", "Keyboard"]
